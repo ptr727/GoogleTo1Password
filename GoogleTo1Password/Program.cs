59bf850a40832f609150d5ab8d78003a31e452c7
Pieter Viljoen <pieter_viljoen@symantec.com>
2018-09-12 12:23:17 -0700

    Adding commandline input support

--- GoogleTo1Password/Program.cs
+++ GoogleTo1Password/Program.cs
@@ -1,5 +1,8 @@
 ﻿using CsvHelper;
+using CsvHelper.Configuration;
+using McMaster.Extensions.CommandLineUtils;
 using System;
+using System.ComponentModel.DataAnnotations;
 using System.IO;
 using System.Net;
 
@@ -7,56 +10,89 @@ namespace GoogleTo1Password
 {
     class Program
     {
+        // E.g. -InputFile "C:\Users\piete\OneDrive\1Password\Chrome Passwords.csv"
+        public static int Main(string[] args) => CommandLineApplication.Execute<Program>(args);
+
+        [Option("-InputFile")]
+        [Required]
+        string InputFile { get; }
+
         class Login
         {
-            public string name { get; set; }
-            public string url { get; set; }
-            public string username { get; set; }
-            public string password { get; set; }
+            public string Name { get; set; }
+            public string Url { get; set; }
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }
+        class LoginMap : ClassMap<Login>
+        {
+            public LoginMap()
+            {
+                AutoMap();
+                Map(m => m.Name).Name("name");
+                Map(m => m.Url).Name("url");
+                Map(m => m.Username).Name("username");
+                Map(m => m.Password).Name("password");
+            }
         }
 
-        static void Main(string[] args)
+
+
+        private int OnExecute()
         {
+            string inputCsv = InputFile;
+            Console.WriteLine($"Input File Name : {inputCsv}");
+
             // Follow instructions
             // https://support.1password.com/import-chrome/
 
             // Read input CSV file
-            string inputCsv = @"C:\Users\piete\OneDrive\1Password\Chrome Passwords.csv";
             TextReader reader = new StreamReader(inputCsv);
             CsvReader csvReader = new CsvReader(reader);
+            csvReader.Configuration.RegisterClassMap<LoginMap>();
             var records = csvReader.GetRecords<Login>();
 
             // Open CSV output file
-            string outputCsv = inputCsv + "_filtered.csv";
+            string outputCsv = Path.ChangeExtension(inputCsv, ".conv.csv");
+            Console.WriteLine($"Output File Name : {outputCsv}");
             TextWriter writer = new StreamWriter(outputCsv);
             CsvWriter csvWriter = new CsvWriter(writer);
+            csvWriter.Configuration.RegisterClassMap<LoginMap>();
             csvWriter.WriteHeader<Login>();
             csvWriter.NextRecord();
 
             // Process all records
+            int input = 0, output = 0;
             foreach (Login login in records)
             {
+                input ++;
+
                 // Skip entries for IP addresses
                 IPAddress ip;
-                if (IPAddress.TryParse(login.name, out ip))
+                if (IPAddress.TryParse(login.Name, out ip))
                     continue;
 
                 // Skip entries where the username or password field contains ***
-                if (login.username.Contains("***") || login.password.Contains("***"))
+                if (login.Username.Contains("***") || login.Password.Contains("***"))
                     continue;
 
                 // Crack the URI and use only the domain name
-                Uri uri = new Uri(login.url);
-                login.url = $"{uri.Scheme}://{uri.Host}";
+                Uri uri = new Uri(login.Url);
+                login.Url = $"{uri.Scheme}://{uri.Host}";
 
                 // Write the record
                 csvWriter.WriteRecord(login);
                 csvWriter.NextRecord();
+                output ++;
             }
 
             // Close
             reader.Close();
             writer.Close();
+
+            Console.WriteLine($"Wrote {output} of {input} records.");
+
+            return 0;
         }
     }
 }
